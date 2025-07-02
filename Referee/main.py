f10297f6a84e6e7843c130578adfb27e654a5599
thyildirim <taha0yldrm@outlook.com>
2025-07-02 22:58:34 +0300

    fix

--- Referee/main.py
+++ Referee/main.py
@@ -4,14 +4,16 @@ from pymongo import MongoClient
 app = FastAPI()
 
 client = MongoClient('mongodb://db:27017/')
-db = client['Game']
-collection = db['Scores']
-
+db = client['Game']  #db
+collection = db['Scores']  #table   
 
 @app.get("/scores")
 def read_scores():
-    score_dict = {}
-    for score in collection.find(): 
-        score_dict[score['container_name']] = score['score']
-    return score_dict
 
+    score_dict = {}
+    
+    for score in collection.find(): 
+
+        score_dict[score['container_name']] = score['score']
+    
+    return score_dict
